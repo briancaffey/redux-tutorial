4bcd0ed1e7b4d7aa1dc78c2fc8739f8ffde6854c
Brian Caffey <briancaffey2010@gmail.com>
2017-07-07 19:12:05 -0400

    added a special case to next that returns the winner

--- src/core.js
+++ src/core.js
@@ -17,6 +17,11 @@ function getWinners(vote){
 export function next(state) {
   const entries = state.get('entries')
                        .concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
   return state.merge({
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
